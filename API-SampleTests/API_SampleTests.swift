86137b56112e5a41969825aa4c84873d1ce46dcc
tosh7 <zlia.6.lj.425@gmail.com>
2018-08-26 18:44:35 +0900

    Initial Commit

---
+++ API-SampleTests/API_SampleTests.swift
@@ -0,0 +1,36 @@
+//
+//  API_SampleTests.swift
+//  API-SampleTests
+//
+//  Created by Satoshi Komatsu on 2018/08/26.
+//  Copyright © 2018年 Satoshi Komatsu. All rights reserved.
+//
+
+import XCTest
+@testable import API_Sample
+
+class API_SampleTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
